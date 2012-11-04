800ed839dd91d5b8f41990c548da3934d55663b4
Chad Etzel <jazzychad@gmail.com>
2012-11-04 13:43:28 -0800

    Initial Commit

---
+++ CEGuideArrow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CEGuideArrow
+//
+//  Created by Chad Etzel on 11/4/12.
+//  Copyright (c) 2012 Chad Etzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CEGuideArrow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CEGuideArrow
+//
+//  Created by Chad Etzel on 11/4/12.
+//  Copyright (c) 2012 Chad Etzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
