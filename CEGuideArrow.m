847025d72a310dc0d1125c5d5007bd85fedecb0c
Chad Etzel <jazzychad@gmail.com>
2012-11-04 17:17:58 -0800

    update license, add readme

--- CEGuideArrow.m
+++ CEGuideArrow.m
@@ -1,10 +1,31 @@
 //
 //  CEGuideArrow.m
-//  Pixit
 //
-//  Created by Charles Etzel on 7/22/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Created by Chad Etzel on 7/22/12.
+//  Copyright (c) <2012> Chad Etzel
 //
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
 
 #import "CEGuideArrow.h"
 
@@ -58,6 +79,18 @@ static CEGuideArrow *_sharedGuideArrow;
     return CGPointMake(1.0, 0.5);
 }
 
+- (void)removeCompleted
+{
+    [_arrowView removeFromSuperview];
+    
+    if ([self.delegate respondsToSelector:@selector(ceGuideArrow:didDisappearInWindow:atPoint:inView:length:)]) {
+        [self.delegate ceGuideArrow:self didDisappearInWindow:_window atPoint:_point inView:_view length:_length];
+    }
+    
+    _displayed = NO;
+    [_arrowView.layer removeAnimationForKey:@"bounceAnimation"];
+}
+
 - (void)removeAnimated:(BOOL)animated
 {
     if ([self.delegate respondsToSelector:@selector(ceGuideArrow:willDisappearInWindow:atPoint:inView:length:)]) {
@@ -72,27 +105,13 @@ static CEGuideArrow *_sharedGuideArrow;
             
         } completion:^(BOOL finished) {
             
-            [_arrowView removeFromSuperview];
-            
-            if ([self.delegate respondsToSelector:@selector(ceGuideArrow:didDisappearInWindow:atPoint:inView:length:)]) {
-                [self.delegate ceGuideArrow:self didDisappearInWindow:_window atPoint:_point inView:_view length:_length];
-            }
-            
-            _displayed = NO;
-            [_arrowView.layer removeAnimationForKey:@"bounceAnimation"];
+            [self removeCompleted];
             
         }];
         
     } else {
         
-        [_arrowView removeFromSuperview];
-        
-        if ([self.delegate respondsToSelector:@selector(ceGuideArrow:didDisappearInWindow:atPoint:inView:length:)]) {
-            [self.delegate ceGuideArrow:self didDisappearInWindow:_window atPoint:_point inView:_view length:_length];
-        }
-        
-        _displayed = NO;
-        [_arrowView.layer removeAnimationForKey:@"bounceAnimation"];
+        [self removeCompleted];
         
     }
 }
@@ -123,7 +142,7 @@ static CEGuideArrow *_sharedGuideArrow;
     windowPoint = CGPointMake(windowRect.origin.x + point.x, windowRect.origin.y + point.y);
     
     
-    //// animation
+    //// bounce animation
 
     [_arrowView.layer removeAnimationForKey:@"bounceAnimation"];
     
---
+++ README.md
@@ -0,0 +1,42 @@
+# CEGuideArrow
+
+A custom iOS UI control which allows display of an arrow image to
+point at arbitrary locations in a window at arbitrary angles and
+lengths.
+
+## Motivation
+
+I created the CEGuideArrow class for an unlaunched side-project in
+which I wanted to step through a short tutorial in the app. I needed a
+flexible way to guide the user along and point at the next steps along
+the work flow. Thus, CEGuideArrow was born, and now I'm giving it to
+the world.
+
+## Demo
+
+You can view a quick video demo of the guide arrow at work in the
+[CEGuideArrow Demo YouTube
+Video](http://www.youtube.com/watch?v=U6sdXCZmmMM).
+
+There is a demo XCode project included with this repo to demonstrate
+use of the class.
+
+## Documentation
+
+There is not much in the way of documentation yet (maybe ever?), but
+the demo project demonstrates most of the class's capabilities in
+simple code
+
+## What, no ARC?
+
+I'm old school. You can very happily and easily integrate non-ARC code
+into ARC projects using the `-fno-objc-arc` flag in the "Compile
+Sources" step of the project "Build Phases" - any pull requests or
+issues asking to integrate ARC will be rejected with prejudice.
+
+## Contributing
+
+Want to contribute and make CEGuideArrow even better? Great! Send a
+pull request my way, and I'll check it out.
+
+Happy Hacking!
